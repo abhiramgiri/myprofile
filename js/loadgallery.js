6d7ed7c1130a2fce927a269fc4ee6acddbd57868
Abhiram Giri <abhiram.giri@gmail.com>
2020-06-20 03:42:03 +0530

    secure2

--- js/loadgallery.js
+++ js/loadgallery.js
@@ -22,10 +22,14 @@
 // });
 
 $(document).ready(function () {
+    var cls="col-6 col-sm-4 col-md-2 col-lg-2";
+    if($('#lightgallery').hasClass("hs")){
+        cls="item";
+    }
     var i;
         var content="";
         for (i = 0; i < 28; i++) {    
-            content += '<li class="col-xs-6 col-sm-4 col-md-2 col-lg-2" data-responsive="./galleryimages/thumbnail/IMAGE_'+i+'.jpg" data-src="./galleryimages/view/IMAGE_'+i+'.jpg" data-download-url="./galleryimages/download/IMAGE_'+i+'.JPG"><a href=""><img class="img-responsive" src="./galleryimages/thumbnail/IMAGE_'+i+'.jpg"></a></li>';
+            content += '<li class="'+cls+'" data-responsive="./galleryimages/thumbnail/IMAGE_'+i+'.jpg" data-src="./galleryimages/view/IMAGE_'+i+'.jpg" data-download-url="./galleryimages/download/IMAGE_'+i+'.JPG"><a href=""><img class="img-responsive" src="./galleryimages/thumbnail/IMAGE_'+i+'.jpg"></a></li>';
         }        
         $('#lightgallery').html(content);
     $('#lightgallery').lightGallery(); 
